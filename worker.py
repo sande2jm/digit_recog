20eb1ca6ad459867b065a8bd236681d5249cccc1
sande2jm <sande2jm@gmail.com>
2018-07-11 05:10:34 -0400

    version 9.4

--- worker.py
+++ worker.py
@@ -26,11 +26,13 @@ class My_callback(keras.callbacks.Callback):
     	pass
 
     def on_batch_end(self, batch, logs={}):
+        	while self.state == 'pause':
+        		time.sleep(.3)
         	d = {
             'message': "working",
             'id': self.my_id,
             'state': self.state,
-            'progress': round((batch*32/(42000)*4),4)}
+            'progress': round((batch*32/(42000*4),4)}
         	response = self.queue.send_message(MessageBody=json.dumps(d), MessageGroupId='model_bots')       
         	# self.report(batch, size=42000)
 	        # while self.state == 'pause':
