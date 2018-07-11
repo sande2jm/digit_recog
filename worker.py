cfa44b3295b3e309ccadedae361e36a697648d38
sande2jm <sande2jm@gmail.com>
2018-07-11 01:59:06 -0400

    version 7.0

--- worker.py
+++ worker.py
@@ -39,8 +39,8 @@ class My_callback(keras.callbacks.Callback):
 			'message': self.state,
 			'id': self.my_id,
 			'progress': round((i*32/(size)*4),4)
-	        }
-	        response = self.queue.send_message(MessageBody=json.dumps(d), MessageGroupId='model_bots')	        
+			}
+		response = self.queue.send_message(MessageBody=json.dumps(d), MessageGroupId='model_bots')	        
 
 class Worker():
 
